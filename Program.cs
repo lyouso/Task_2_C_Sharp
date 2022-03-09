b9b5d48569e511899779bc88deaa6261a92d8c9a
Liubov K <lyouso@yandex.ru>
2022-03-09 16:25:08 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) Console.WriteLine("Max = " + a);
+else Console.WriteLine("Max = " + b); 
+if (a == b) Console.WriteLine("Вы ввели равные числа");
