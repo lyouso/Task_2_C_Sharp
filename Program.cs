fd443cfdda342cc2ecac1c4d7337c4c38edbf1b1
Liubov K <lyouso@yandex.ru>
2022-03-16 13:59:09 +0300

    Changed program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b) Console.WriteLine("Число " + a + " больше числа " + b);
-else Console.WriteLine("Число " + b + " больше числа " + a); 
 if (a == b) Console.WriteLine("Вы ввели равные числа");
+else {
+    if (a > b) Console.WriteLine("Число " + a + " больше числа " + b);
+    else Console.WriteLine("Число " + b + " больше числа " + a); 
+}
